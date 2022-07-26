ba7d4a061a4128d90e305fce26b7e1706433fed2
sarbeswarprusty <109785239+sarbeswarprusty@users.noreply.github.com>
2022-07-26 08:53:05 +0530

    Add files via upload

---
+++ mainwindow.cpp
@@ -0,0 +1,24 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include "dialog.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    Dialog dialog;
+    dialog.setModal(true);
+    dialog.exec();
+}
+
